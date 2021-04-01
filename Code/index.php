74d6c17b04be49447b2eb67303870fde27d51fc6
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-04-01 19:46:23 +0100

    styling the page

--- Code/index.php
+++ Code/index.php
@@ -3,17 +3,42 @@
     $sql = "SELECT * FROM projects";
     $resultat = mysqli_query($link,$sql);
 
+    $sqli = "INSERT INTO `validation` "
+
     
 ?>
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styletst.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="student_style.css">
+    <title>class management app</title>
 </head>
+<body method="POST" action="">
+    <header class="container">
+        <div class="logo"><h1 class="you">You</h1>
+        <h1 class="code">Code</h1></div>
+        <ol class="navitems">
+            <li><a href="#">Briefs</a></li>
+            <li><a href="#">works</a></li>
+        </ol>
+        
+            <ul class="navitems2">
+                <li  class=profil>
+                    <img src="person-male.png" alt="#">
+                    <a href="#">Profil</a></li>                                           
+                <li class="logout" >
+                    <form action="logout.php"method="POST">
+                        <input type="submit" class="logout"  name="LOGOUT" value="logout">
+                    </form>
+                </li>                                           
+            </ul>
+       
+    </header>
+
 <body>
     <table>
         <thead>
@@ -32,7 +57,7 @@
                         <td><?= $row['titre_projet'];?></td>
                         <td><?= $row['nateur de projet'];?></td>
                         <td><?= $row['urlt'];?></td>
-                        <td><input type="text" name="rendu"><button>push</button> </td>
+                        <td><input type="text" name="rendu"><button name="push" type="submit">push</button> </td>
                     </tr>
                 <?php endforeach ?>
                 
