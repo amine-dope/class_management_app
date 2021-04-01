28f88e6e815d632160799f96d61125eb0fb52d0a
ilyess el kahia <ilyess.elkahia@gmail.com>
2021-04-01 15:21:06 +0100

    backend login étudiant et formateur

--- Code/connexion.php
+++ Code/connexion.php
@@ -5,21 +5,32 @@ session_start();
 
 
 
+
+
+
 if (isset($_POST["login"])) {
 
     $user = $_POST['username'];
     $mdp = $_POST['password'];
-
+    $req2 = ("SELECT * FROM formateur WHERE username = '$user' and motdepasse = '$mdp'");
+    $pro2 = mysqli_query($link, $req2);
+    $row2 = mysqli_num_rows($pro2);
     $req = ("SELECT * FROM etudiant WHERE username = '$user' and motdepasse = '$mdp' ");
     $pro = mysqli_query($link, $req);
     $row = mysqli_num_rows($pro);
 
-    if ($row == 0) {
-        echo "Quelque chose est incorrect!";
-    } else {
-        $_SESSION['username'] = $row['username'];
-        $_SESSION['id'] = $row['id'];
 
-        echo "ok";
+    if ($row == 0 && $row2 == 0) {
+        echo "Quelque chose est incorrect!";
+    } elseif ($row == 1) {
+        echo "Etudiant";
+    } elseif ($row2 == 1) {
+        echo "Formateur";
     }
+    // else {
+    //     $_SESSION['username'] = $row['username'];
+    //     $_SESSION['id'] = $row['id'];
+
+    //     echo "ok";
+    // }
 }
