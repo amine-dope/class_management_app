b0b4536dc3af55eeadc0d53551299b46aef0e3f0
chaima marzouk <marzouk.chaima.892@gmail.com>
2021-04-02 10:47:00 +0100

    insertion du rendu sur DB

--- Code/traitement.php
+++ Code/traitement.php
@@ -3,5 +3,18 @@
 include_once('database.php');
 
 
-$insert = ("INSERT INTO `validation`( `url`) VALUES ([value-1]");
-$query =  mysqli_query($link,$insert);
+
+if (isset($_POST['push'])) {
+   
+
+
+if (empty($_POST['rendu'])) {
+    echo "Please enter your project URL";
+}
+
+else if (!empty($_POST['rendu'])) {
+    $insert = ("INSERT INTO `validation`( `url`) VALUES ('" . $_POST['rendu'] . "')");
+    $query =  mysqli_query($link,$insert);
+    header('location:student_index.php');
+}
+}
